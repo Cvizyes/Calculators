f05a0baf3341219b1c486121720a25de44cc6810
Cvizyes <carlosvizcaino718@gmail.com>
2018-01-05 13:08:54 -0400

    test are added

--- test/unit/specs/calculador.spec.js
+++ test/unit/specs/calculador.spec.js
@@ -7,5 +7,83 @@ describe('calculator.vue', () => {
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
     .to.equal('Welcome to Your calculator app')
-  })
+  });
+  it('should run press button ',function (press){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run clear button',function (clear){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run backSpace button',function (backSpace){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run multiply button',function (multiply){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run divide button',function (divide){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run plus button',function (plus){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run minus button',function (minus){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run sin button',function (sin){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run cos',function (cos){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run',function (tan){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run log button',function (log){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run squareRoot button',function (squareRoot){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
+    it('should run square',function (square){
+    const button = calculatorCopenent.$el.querySelector('button');
+    const clickEvent = new window.Event('click');
+    button.dispatchEvent(clickEvent);
+    calculatorCopenent._watcher.run();
+  });
 })
